fea81684ccf28f7016d91cf70da06b7dc797cfe0
ManChlenemus <ygu_info@mail.ru>
2025-03-03 05:58:35 +0300

    test

--- test.bat
+++ test.bat
@@ -1,16 +1,24 @@
 @echo off
+REM Очищаем переменную output
+SET output=
 
-REM Запускаем программу и сохраняем вывод в переменную
-FOR /F "tokens=*" %%A IN ('jenkins-test') DO (
-    SET output=%%A
+REM Запускаем программу и сохраняем весь вывод в переменную
+FOR /F "delims=" %%A IN ('jenkins-test') DO (
+    SET "output=%%A"
 )
 
-SET expected=hello
+REM Обходим проблему с расширением переменных внутри IF
+SET "expected=hello"
 
-IF "%output%"=="%expected%" (
+REM Включаем расширенное расширение переменных
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+IF "!output!"=="!expected!" (
     echo Test passed!
-    exit 0
+    exit /b 0
 ) ELSE (
     echo Test failed!
-    exit 1
+    exit /b 1
 )
+
+ENDLOCAL
